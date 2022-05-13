b467e4196462444ee4e5dfd11de63a47b971817a
robertkatz1971 <83724236+robertkatz1971@users.noreply.github.com>
2022-05-13 00:23:53 -0400

    added navigation framework

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
-import './components/category-item/category-item.component.jsx'
-import Directory from './components/directory/directory.component.jsx';
+import { Routes, Route} from 'react-router-dom';
+import Home from './components/routes/home/home.component.jsx';
+import Navigation from './components/routes/navigation/navigation.component.jsx';
+
+const Shop = () => {
+  return <h1>Shop Page</h1>
+}
 
 const App = () => {
 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ];
-
   return (
-    <Directory categories={categories} />
+    <Routes>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
+    </Routes>
   );
   
-}
+};
 
 export default App;
